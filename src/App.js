5dd91cfc8cd4215c19aeac82441ad829e4c47c28
dipak <sarkar3115@gmail.com>
2023-05-02 17:43:09 +0530

    done

--- src/App.js
+++ src/App.js
@@ -4,24 +4,37 @@ import Workcard from './Workcard';
 import {data} from './data';
 import { useState } from 'react';
 
+//use effect when frame
+
 function App() {
   const [tframe, setTframe] = useState("Week");
-  const [newdata, setNewdata] = useState([]);
+  const [newdata, setNewdata] = useState([
+    {current: 32, previous: 36},
+    {current: 10, previous: 8},
+    {current: 4, previous: 7},
+    {current: 4, previous: 5},
+    {current: 5, previous: 10}, 
+    {current: 2, previous: 2}
+  ]);
 
+  let data2 =[] ;
   const handleDaily = ()=>{
-    setTframe("Day");
-    setNewdata(data.map(data => data.timeframes.daily));
-    console.log(newdata);
+    const frame = 'Day';
+    setTframe(frame);
+    data2 = data.map(data => data.timeframes.daily); 
+    setNewdata(data2);
   };
   const handleWeekly = ()=>{
-    setTframe("Week");
-    setNewdata(data.map(data => data.timeframes.weekly));
-    console.log(newdata);
+    const frame = 'Week';
+    setTframe(frame);
+    data2 = data.map(data => data.timeframes.weekly);
+    setNewdata(data2); 
   };
   const handleMonthly = ()=>{
-    setTframe("Month");
-    setNewdata(data.map(data => data.timeframes.monthly));
-    console.log(newdata);
+    const frame = 'Month';
+    setTframe(frame);
+    data2 = data.map(data => data.timeframes.monthly); 
+    setNewdata(data2);
   };
 
   return (
@@ -42,32 +55,32 @@ function App() {
       </section>
       <section className='tt-work tt-sec'>
         <div className='tt-sec-info'>
-          <Workcard crt={32} prv={36} cname={"Work"} tint={tframe}/>
+          <Workcard crt={newdata[0].current} prv={newdata[0].previous} cname={"Work"} tint={tframe}/>
         </div>
       </section>
       <section className='tt-play tt-sec'>
         <div className='tt-sec-info'>
-          <Workcard crt={10} prv={8} cname={"Play"} tint={tframe}/>
+          <Workcard crt={newdata[1].current} prv={newdata[1].previous} cname={"Play"} tint={tframe}/>
         </div>
       </section>
       <section className='tt-study tt-sec'>
         <div className='tt-sec-info'>
-          <Workcard crt={4} prv={7} cname={"Study"} tint={tframe}/>
+          <Workcard crt={newdata[2].current} prv={newdata[2].previous} cname={"Study"} tint={tframe}/>
         </div>
       </section>
       <section className='tt-exercise tt-sec'>
         <div className='tt-sec-info'>
-          <Workcard crt={4} prv={5} cname={"Exercise"} tint={tframe}/>
+          <Workcard crt={newdata[3].current} prv={newdata[3].previous} cname={"Exercise"} tint={tframe}/>
         </div>
       </section>
       <section className='tt-social tt-sec'>
         <div className='tt-sec-info'>
-          <Workcard crt={5} prv={10} cname={"Social"} tint={tframe}/>
+          <Workcard crt={newdata[4].current} prv={newdata[4].previous} cname={"Social"} tint={tframe}/>
         </div>
       </section>
       <section className='tt-selfcare tt-sec'>
         <div className='tt-sec-info'>
-          <Workcard crt={2} prv={2} cname={"Self Care"} tint={tframe}/>
+          <Workcard crt={newdata[5].current} prv={newdata[5].previous} cname={"Self Care"} tint={tframe}/>
         </div>
       </section>
     </article>
